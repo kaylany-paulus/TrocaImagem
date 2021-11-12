1d40b6fe9bbf7d8836cb9bba6860a0dc4d50e305
kaylany-paulus <kaylanysilvapaulus@gmail.com>
2021-11-11 22:00:52 -0300

    modificação inicial

---
+++ script.js
@@ -0,0 +1,14 @@
+function mudarImagem() {
+    var text = window.document.getElementById('texto');
+    text.style.fontSize = '30px';
+    text.innerText = "QUE ISSO GENTE";
+    text.style.transition = "all 1.7s";
+    var imagem = window.document.getElementById('imagem');
+    imagem.src = "Imagens/Que isso.jpg/";
+    imagem.style.border = "2px solid black"
+    var cor = window.document.getElementById('container');
+    cor.style.backgroundColor = 'rgb(70, 70, 70)';
+    cor.style.transition = "all 1.7s";
+    cor.style.border = "7px solid #332F27";
+
+}
