c49d77826bde36a4fa79045ff7d84c549a86d9f3
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-11-15 23:19:08 -0300

    Alteração nas imagens

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 function mudarImagem() {
     var text = window.document.getElementById('texto');
     text.style.fontSize = '30px';
-    text.innerText = "QUE ISSO GENTE";
+    text.innerText = "QUE ISSO GENTE!!";
     text.style.transition = "all 1.7s";
     var imagem = window.document.getElementById('imagem');
-    imagem.src = "Imagens/Que isso.jpg/";
+    imagem.src = "img/AlanPerfil.jpg";
     imagem.style.border = "2px solid black"
     var cor = window.document.getElementById('container');
     cor.style.backgroundColor = 'rgb(70, 70, 70)';
     cor.style.transition = "all 1.7s";
     cor.style.border = "7px solid #332F27";
+
 
 }
