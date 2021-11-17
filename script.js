a93336b58cb78b3ea02f2fbb95c24190ac2cbb4b
Kaylany Paulus <kaylanysilvapaulus@gmail.com>
2021-11-17 10:58:36 -0300

    alteração no backgroundColor da segunda img

--- script.js
+++ script.js
@@ -7,9 +7,7 @@ function mudarImagem() {
     imagem.src = "img/AlanPerfil.jpg";
     imagem.style.border = "2px solid black"
     var cor = window.document.getElementById('container');
-    cor.style.backgroundColor = 'rgb(70, 70, 70)';
+    cor.style.backgroundColor = 'rgb(121, 6, 17)';
     cor.style.transition = "all 1.7s";
-    cor.style.border = "7px solid #332F27";
-
 
 }
